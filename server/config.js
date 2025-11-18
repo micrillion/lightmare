9e6d32578560935f8b718df5daa26b29b7e84988
micrillion <tiffanylward76@gmail.com>
2025-11-17 19:33:13 -0500

    Update config.js

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ module.exports = {
 
     // Game server domain.
     // If the host is 'localhost:NUMBER', the NUMBER must be the port setting.
-    host: "https://micrillion.github.io/lightmare",
+    host: "micrillion.github.io",
 
     // Which port to run the web server on.
     port: 3000,
